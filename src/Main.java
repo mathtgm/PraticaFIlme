bed234a8f0be2b4b04b7c6580f0e305e776fab78
Matheus Gabriel <matheusg663@gmail.com>
2023-11-28 20:05:24 -0300

    Implementação de pacotes e métodos de Getters e Setters na classe Filme

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import br.com.filme.modelo.Filme;
+
 public class Main {
     public static void main(String[] args) {
 
         // Declaração Filme 1
         Filme filme1 = new Filme();
 
-        filme1.nome = "Jogos Vorazes e o Cantigo da serpente e do passáro";
-        filme1.anoDeLancamento = 2023;
-        filme1.duracaoEmMinutos = 230;
-        filme1.incluidoNoPlano = true;
+        filme1.setNome("Jogos Vorazes e o Cantigo da serpente e do passáro");
+        filme1.setAnoDeLancamento(2023);
+        filme1.setDuracaoEmMinutos(230);
+        filme1.setIncluidoNoPlano(true);
 
         filme1.avalia(4.2);
         filme1.avalia(5.0);
@@ -16,10 +18,10 @@ public class Main {
         // Declaração Filme 2
         Filme filme2 = new Filme();
 
-        filme2.nome = "A freira";
-        filme2.anoDeLancamento = 2023;
-        filme2.duracaoEmMinutos = 150;
-        filme2.incluidoNoPlano = false;
+        filme2.setNome("A freira");
+        filme2.setAnoDeLancamento(2023);
+        filme2.setDuracaoEmMinutos(150);
+        filme2.setIncluidoNoPlano(false);
 
         filme2.avalia(5.0);
         filme2.avalia(5.0);
